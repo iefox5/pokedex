d62844ae834699356642b950964d63ca2064f0a4
Yang Pei <iefox5@Yangs-MacBook-Pro.local>
2016-01-20 23:30:51 +0800

    created class

---
+++ Pokedex/pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  pokemon.swift
+//  Pokedex
+//
+//  Created by Yang Pei on 1/20/16.
+//  Copyright © 2016 Yang Pei. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+    
+}
